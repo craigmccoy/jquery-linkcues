1a95bed919eebd44846195dce376895abca19bd9
Craig McCoy <craigmccoy@gmail.com>
2012-02-13 09:06:48 -0600

    added quotes in selectors

--- jquery.linkcues.js
+++ jquery.linkcues.js
@@ -2,12 +2,12 @@
     $.fn.linkcues = function(options) {
 		var settings = $.extend({
 		    filetypes: 'pdf',
-		    classPrefix: ''
+		    classPrefix: 'linkcues'
 		}, options);
 	
 		var container = $(this);
 	
-		$('a[href^=http://],a[href^=https://],a[href^=ftp://]', container).each(function() {
+		$('a[href^="http://"],a[href^="https://"],a[href^="ftp://"]', container).each(function() {
 		    var site = window.location.protocol + '//' + window.location.hostname;
 		    if($(this).attr('href').substr(0, site.length) != site) {
 		    	$(this).addClass(settings.classPrefix + 'external').attr('target', '_blank');
@@ -19,17 +19,17 @@
 		    }
 		});
 
-		$('a[href^=mailto:]', container).addClass(settings.classPrefix + 'mailto').each(function() {
+		$('a[href^="mailto:"]', container).addClass(settings.classPrefix + 'mailto').each(function() {
 		    if($(this).attr('title') == '') {
-		    	$(this).attr('title', 'send e-mail to ' + $(this).attr('href').replace('mailto:', '').split('?')[0]);
+		    	$(this).attr('title', 'send email to ' + $(this).attr('href').replace('mailto:', '').split('?')[0]);
 		    }
 		});
 
 		var filetypes = settings.filetypes.split(',');
 		for(var i = 0; i < filetypes.length; i++) {
-		    $('a[href$=.' + $.trim(filetypes[i]) + ']', container).addClass(settings.classPrefix + $.trim(filetypes[i])).attr('target', '_blank');
+		    $('a[href$=".' + $.trim(filetypes[i]) + '"]', container).addClass(settings.classPrefix + $.trim(filetypes[i])).attr('target', '_blank');
 		}
 
-		$('a[target=_blank]', container).addClass(settings.classPrefix + 'new-window'); 
-    };
+		$('a[target="_blank"]', container).addClass(settings.classPrefix + 'new-window'); 
+    }; 
 })(jQuery);
