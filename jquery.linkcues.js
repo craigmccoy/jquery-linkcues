fb11db64b38d002915cdd2b6d57bb4d02a3dea0d
Craig McCoy <craig@consumerbrands.com>
2012-09-19 09:49:54 -0500

    added a configurable window target

--- jquery.linkcues.js
+++ jquery.linkcues.js
@@ -1,8 +1,10 @@
 (function($) {
+	
     $.fn.linkcues = function(options) {
 		var settings = $.extend({
-		    filetypes: 'pdf',
-		    classPrefix: 'linkcues-'
+		    filetypes : 'pdf',
+		    classPrefix : 'linkcues-',
+		    windowTarget : '_blank'
 		}, options);
 	
 		var container = $(this);
@@ -11,12 +13,12 @@
 		    var site = window.location.protocol + '//' + window.location.hostname;
 		    var link = $(this);
 		    if(link.attr('href').substr(0, site.length) != site) {
-		    	link.addClass(settings.classPrefix + 'external').attr('target', '_blank');
+		    	link.addClass(settings.classPrefix + 'external').attr('target', settings.windowTarget);
 		    }
 		    if(link.attr('href').substr(0, 6) == 'https:') {
 		    	link.addClass(settings.classPrefix + 'secure');
 		    } else if(link.attr('href').substr(0, 4) == 'ftp:') {
-		    	link.addClass(settings.classPrefix + 'ftp').attr('target', '_blank');
+		    	link.addClass(settings.classPrefix + 'ftp').attr('target', settings.windowTarget);
 		    }
 		});
 
@@ -29,11 +31,12 @@
 
 		var filetypes = settings.filetypes.split(',');
 		for(var i = 0; i < filetypes.length; i++) {
-		    $('a[href$=".' + $.trim(filetypes[i]) + '"]', container).addClass(settings.classPrefix + $.trim(filetypes[i])).attr('target', '_blank');
+		    $('a[href$=".' + $.trim(filetypes[i]) + '"]', container).addClass(settings.classPrefix + $.trim(filetypes[i])).attr('target', settings.windowTarget);
 		}
 
-		$('a[target="_blank"]', container).addClass(settings.classPrefix + 'new-window'); 
+		$('a[target="' + settings.windowTarget + '"]', container).addClass(settings.classPrefix + 'new-window'); 
 		
 		return container;
-    }; 
+    };
+    
 })(jQuery);
